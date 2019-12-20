3574c28c77fec9c82b267de7fcbd22e5fb84d52b
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-20 13:26:58 +0200

    brushing up on struct basics

---
+++ struct_test.c
@@ -0,0 +1,21 @@
+#include "./libtlpi/tlpi_hdr.h"
+
+struct X
+{
+	int a;
+};
+
+void set_value(struct X *this, int v)
+{
+	this->a = v;
+};
+
+
+int main(void)
+{
+	struct X x;
+	set_value(&x, 5);
+	printf("%d\n", x.a);
+	exit(EXIT_SUCCESS);
+}
+
