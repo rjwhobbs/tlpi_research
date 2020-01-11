d4b5dcfe6f681e4f1681bbc4ae25196b648b3b81
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-01-11 07:17:07 +0200

    Thanks to u/makiftasova and u/oh5nxo for input on my code.

--- exercises_4/rh_tee.c
+++ exercises_4/rh_tee.c
@@ -1,9 +1,17 @@
 #include <ctype.h>
 #include <fcntl.h>
+#include <stdlib.h> /* for open() */
+#include <sys/resource.h> /* for getrlimit() */
+#include <sys/time.h> /* for getrlimit() */
+#include <unistd.h> /* for close() and getopt() */
 /* 	
 *	include project local headers after system headers. 
 *	This way you will know when your local headers are overwriting 
 *	something from system headers/libraries:
+*	Even though most of the std lib headers are included in 
+*	tlpi_hdr.h is seems to be best practice to explicitly 
+*	include all relavent headers incase you make changes.
+*	This could be very relevant in big projects.
 */
 #include "../libtlpi/tlpi_hdr.h"
 #include "../libft/libft.h"
@@ -13,32 +21,35 @@ int		main(int argc, char *argv[])
 	int opt;
 	struct rlimit lim;
 	unsigned int i;
-	unsigned int fd_buf;
+	unsigned int num_file;
 	int read_check;
 
-	int opt_check = 0;
+	int a_flag = 0;
 	char *line = NULL;
 	int open_flags = O_CREAT | O_WRONLY | O_APPEND |O_TRUNC ;
 	int file_perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | 
 					S_IROTH | S_IWOTH;
 
-	if(argc == 1)
-		return (EXIT_FAILURE);
+	if (argc == 1)
+	{
+		dprintf(STDERR_FILENO, "Usage %s [-a] [file]\n", argv[0]);
+				return (EXIT_FAILURE);
+	}
 
 	while ((opt = getopt(argc , argv, ":a")) != -1)
 	{
 		switch (opt)
 		{
 			case 'a':
-				opt_check = 1;
+				a_flag = 1;
 				break;
 			default:
 				dprintf(STDERR_FILENO, "Usage %s [-a] [file]\n", argv[0]);
-				return(EXIT_FAILURE);
+				return (EXIT_FAILURE);
 		}
 	}
 
-	if (opt_check)
+	if (a_flag)
 		open_flags = open_flags ^ O_TRUNC; 
 	
 	if (getrlimit(RLIMIT_NOFILE, &lim) == -1)
@@ -47,35 +58,36 @@ int		main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}	
 
-	fd_buf = argc - optind;
+	num_file = argc - optind;
 	// Args amount set to soft limit on open fds
-	if (fd_buf > lim.rlim_cur)
+	if (num_file > lim.rlim_cur)
 	{
 		dprintf(STDERR_FILENO, "Please provide less than %lld arguments.", lim.rlim_cur);
-		exit(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-
-	unsigned int fd[fd_buf]; // What is the effect of declaring vars here instead of at the begining of the program?
-	i = 0;
-
-	for ( ; i < (unsigned int)argc; i++)
+	// As long as you are not using C89 or ANSI-C you can define your variables wherever you want.
+	// I would still like to know the effect if any during compilation.
+	unsigned int fd[num_file]; 
+	
+	// Will this cast argc to unsigned in every time or only on the first call?
+	for (i = 0 ; i < (unsigned int)argc; i++) 
 		fd[i] = open(argv[optind + i], open_flags, file_perms);
 
 	while ((read_check = get_next_line(STDIN_FILENO, &line)) != -1)
 	{
 		if (!read_check)
-			exit(EXIT_SUCCESS);
-		i = 0;
-		while (i < fd_buf)
 		{
-			ft_putendl_fd(line, fd[i]); //prints with a newline char
-			i++;
+			for (i = 0; i < num_file; i++)
+				close (fd[i]);
+			return (EXIT_SUCCESS);
 		}
+		for (i = 0; i < num_file; i++)
+			ft_putendl_fd(line, fd[i]);
 		ft_putendl_fd(line, STDOUT_FILENO); //prints with a newline char
 		if (line)
 			ft_strdel(&line); // free and point to NULL
 	}
 	
 	dprintf(STDERR_FILENO, "Error reading from stdin.");
-	exit(EXIT_FAILURE);
+	return (EXIT_FAILURE);
 }
