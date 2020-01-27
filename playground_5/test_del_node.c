1d40429655c113bc6b9792db8049f68f1c2c766a
rjwhobbs <rhobbs@student.wethinkcode.co.za>
2020-01-27 16:49:23 +0000

    Making progress on the possible race condition bug in ft_lstdel

--- playground_5/test_del_node.c
+++ playground_5/test_del_node.c
@@ -5,6 +5,23 @@ void	ft_del(void *content, size_t size) {
 	free(content);
 }
 
+void	ft_lstdel2(t_list **alsti, void (*del)(void *, size_t))
+{
+	t_list **temp;
+
+	if (alsti && *alsti)
+	{
+		while ((*alsti)->next)
+		{
+			temp = alsti;
+			*alsti = (*alsti)->next;
+			del((*temp)->content, (*temp)->content_size);
+			free(*temp);
+			*temp = NULL;
+		}
+	}
+}
+
 int	main(int ac, char **av) {
 
 	t_list *n1;
@@ -19,7 +36,7 @@ int	main(int ac, char **av) {
 
 	ft_putendl(n1->content);
 	n1->next = n2;
-	free(n1);
+	ft_lstdel2(&n1, ft_del);
 	n3 = ft_lstnew("BISCUITS", sizeof("BISCUITS"));
 	ft_putendl(n1->content);
 	return (0);
